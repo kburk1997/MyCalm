1c766bfd9ac17684ea1c11910341023b50495b32
kburk1997 <kburk97@optonline.net>
2017-09-09 20:57:56 -0400

    finish backend

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,40 @@ var app=express();
 
 const giphy_key=process.env.GIPHY_API_KEY;
 
+var tag_list=[
+  'putty',
+  'fidget',
+  'fidget%20spinner',
+  'satisfying',
+  'kinetic%20sand',
+  'slime',
+  'fidget%20cube',
+  'pottery',
+  'paint%20mixing',
+  'oobleck',
+  'corgi',
+  'puppy',
+  'cat',
+  'kitten',
+  'bunny',
+  'golden%20retriever',
+  'mesmerizing',
+  'shiba',
+  'cats',
+  'corgis',
+  'dog',
+  'dogs',
+  'bunnies',
+  'geometry',
+  'breathing%20exercise',
+  'perfect%20loop'
+
+];
+
+var shuffle=require('shuffle-array');
 
 app.get('/gif/:tag', function(req,res){
-  :wvar url='http://api.giphy.com/v1/gifs/random?tag='+req.param('tag')+'&api_key='+giphy_key+'&rating=g';
+  var url='http://api.giphy.com/v1/gifs/random?tag='+req.param('tag')+'&api_key='+giphy_key+'&rating=g';
   var request=http.get(url,function(response){
   var str="";
      response.on('data', function (chunk) {
@@ -16,11 +47,33 @@ app.get('/gif/:tag', function(req,res){
 
              response.on('end', function () {
 
-               console.log(JSON.parse(str).data);
+               //console.log(JSON.parse(str).data);
                res.send(JSON.parse(str).data); 
              });
   });
 
+});
+
+app.get('/startqueue', function(req,res){
+
+  var shuffled_tags=shuffle(tag_list);
+  
+    for(var i = 0; i<shuffled_tags.length; i++){
+   var url='http://api.giphy.com/v1/gifs/random?tag='+shuffled_tags[i]+'&api_key='+giphy_key+'&rating=g';
+  var request=http.get(url,function(response){
+  var str="";
+     response.on('data', function (chunk) {
+                     str += chunk;
+                             });
+
+             response.on('end', function () {
+
+               //console.log(JSON.parse(str).data);
+               res.write("", JSON.parse(str).data); 
+             });
+  });   
+    }
+  
 });
 
 var server = app.listen(8081, function () {
